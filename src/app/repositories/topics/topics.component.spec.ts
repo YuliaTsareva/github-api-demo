d2b0aa66e263a5bdf862e4064a70c3c76e8dca40
Yulia Tsareva <yulia.tcareva@gmail.com>
2017-05-09 20:53:33 +0300

    Unit tests for topics component

--- src/app/repositories/topics/topics.component.spec.ts
+++ src/app/repositories/topics/topics.component.spec.ts
@@ -1,27 +1,90 @@
+import { Component, DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
 
 import { TopicsComponent } from './topics.component';
+import { POPULAR_TOPICS } from './topics';
+
+@Component({
+    template: `
+        <gh-topics [selectedTopic]="topic"></gh-topics>
+    `
+})
+class TestHostComponent {
+    topic = 'Angular';
+}
 
 describe('TopicsComponent', () => {
-  let component: TopicsComponent;
-  let fixture: ComponentFixture<TopicsComponent>;
+    let fixture: ComponentFixture<TestHostComponent>;
+    let hostComponent: TestHostComponent;
+    let topicsComponent: TopicsComponent;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TopicsComponent ],
-      imports: [RouterTestingModule]
-    })
-    .compileComponents();
-  }));
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [TestHostComponent, TopicsComponent],
+            imports: [RouterTestingModule]
+        });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TopicsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(TestHostComponent);
+        hostComponent = fixture.componentInstance;
+        fixture.detectChanges();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        const topicsDebugElement = fixture.debugElement.query(By.css('gh-topics'));
+
+        topicsComponent = topicsDebugElement.componentInstance;
+    });
+
+    it('should show list of topics', () => {
+        const topics = getTopicElements();
+
+        expect(topics.length).toBe(POPULAR_TOPICS.length);
+    });
+
+    it('should show selected topic', () => {
+        expectSelectedTopicIs('Angular');
+    });
+
+    it('should change selected topic if new topic passed', () => {
+        hostComponent.topic = 'Webpack';
+        fixture.detectChanges();
+
+        expectSelectedTopicIs('Webpack');
+    });
+
+    it('should compare topics case-insensitive', () => {
+        hostComponent.topic = 'webpack';
+        fixture.detectChanges();
+
+        expectSelectedTopicIs('Webpack');
+    });
+
+    it('should update query params on click', async(() => {
+        const topics = getTopicElements();
+
+        topics[0].nativeElement.click();
+        fixture.detectChanges();
+
+        fixture.whenStable().then(() => {
+            const route = TestBed.get(ActivatedRoute);
+
+            expect(route.snapshot.queryParams['topic']).toBe('JavaScript');
+        });
+    }));
+
+    function getTopicElements(): DebugElement[] {
+        return fixture.debugElement.queryAll(By.css('.topic'));
+    }
+
+    function getSelectedTopicElements(): DebugElement[] {
+        return fixture.debugElement.queryAll(By.css('.topic.selected'));
+    }
+
+    function expectSelectedTopicIs(expectedTopic: string) {
+        const selectedTopics = getSelectedTopicElements();
+
+        expect(selectedTopics.length).toBe(1);
+        expect(selectedTopics[0].nativeElement.innerText.trim()).toBe(expectedTopic);
+    }
 });
